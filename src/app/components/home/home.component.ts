bd57aad308f7bb2a32d0bb0a95b4875a58bc54cc
Michael Beaseley <m.t.beaseley@gmail.com>
2018-09-09 19:47:51 +0100

    dev: updated photo links

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,11 +8,9 @@ import { SlideshowModule } from 'ng-simple-slideshow';
 })
 export class HomeComponent implements OnInit {
   imageUrls = [
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/3CNwk7GcU8eEyQUcg4EsOg/16882f10db8b23fbea629b4ed733b946/Profle_pic_Carousel.jpg'},
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/57QD3oI2tiUMOmwqyOgI42/00fc39a5f487a669472d74ff0b2d8667/profile_station2.jpg'},
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/3s2QTQy7ryM46G2ewIesO2/01176aa8aedb1e2faa185bfe1bb32b19/profile_new.jpg'},
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/2tbgM7o5Wk6208QeuQM6o0/fa5ad3300b5da1235436ae75ac195386/profile_pic.jpg'},
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/pj0mHN9DtQUg4gOuqm8KE/77f738415e509ddcbca4786797a9bed9/profile_station.jpg'}
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/3CNwk7GcU8eEyQUcg4EsOg/0ed612b23aa7f2f69408e723cb1a83fe/Profle_pic_Carousel.jpg'},
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/57QD3oI2tiUMOmwqyOgI42/aa1f6fc07bd425a6a7cba65f84f10710/profile_station2.jpg'},
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/2tbgM7o5Wk6208QeuQM6o0/e40591acca2af7f3eb835a1d3bc57fc2/profile_pic.jpg'}
   ];
 
   ngOnInit() {
