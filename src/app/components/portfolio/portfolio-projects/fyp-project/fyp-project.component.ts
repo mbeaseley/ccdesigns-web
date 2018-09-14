5cff0ed3d1e4a1b8db4bd29f438e595928afecd6
Michael Beaseley <m.t.beaseley@gmail.com>
2018-09-14 16:02:02 +0100

    dev: add components for portfolio projects

---
+++ src/app/components/portfolio/portfolio-projects/fyp-project/fyp-project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fyp-project',
+  templateUrl: './fyp-project.component.html',
+  styleUrls: ['./fyp-project.component.scss']
+})
+export class FypProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
