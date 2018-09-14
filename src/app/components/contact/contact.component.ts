4ab7e5232585607f9e25bde67943a0c170f56da2
Michael Beaseley <m.t.beaseley@gmail.com>
2018-09-14 11:10:00 +0100

    dev: make slideshow change to contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -8,9 +8,8 @@ import { SlideshowModule } from 'ng-simple-slideshow';
 })
 export class ContactComponent implements OnInit {
   imageUrls = [
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/3s2QTQy7ryM46G2ewIesO2/98e50163a5281ceda24048302fdb3b6e/profile_new.jpg'},
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/2tbgM7o5Wk6208QeuQM6o0/e40591acca2af7f3eb835a1d3bc57fc2/profile_pic.jpg'},
     {url: 'https://images.ctfassets.net/43i5e5k8e66i/pj0mHN9DtQUg4gOuqm8KE/c4b222cd1703b4f608d68fabc4ccef0a/profile_station.jpg'},
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/2tbgM7o5Wk6208QeuQM6o0/e40591acca2af7f3eb835a1d3bc57fc2/profile_pic.jpg'}
   ];
 
   ngOnInit() {}
