797fb6191a84f2e7a50e418b6696bf3f8a8a29fc
Michael Beaseley <m.t.beaseley@gmail.com>
2018-09-14 11:12:24 +0100

    dev: make portfolio content to porfolio component

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -8,10 +8,13 @@ import { SlideshowModule } from 'ng-simple-slideshow';
 })
 export class PortfolioComponent implements OnInit {
   imageUrls = [
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/4H1rYzMHlYygico2icMSSQ/067d1fc831f04c96ab48e16583f0b979/web_portfolio.JPG'},
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/4O1Qth30iI6uOgQy88eoEi/9e82a8072453dadd927df09a146be1c2/website_pic.jpg'},
     {url: 'https://images.ctfassets.net/43i5e5k8e66i/3JfTj0C6VOcom8684eu6A8/17441f5aab777055e63b1ce9c20339b3/d940d393-ac86-4516-9f6c-52bf33b05852.jpeg'},
   ];
-
+  portfolioImages = [
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/J6F5SWf6uWQ2yAGsSayMM/c0c9daa6fda2775a0494c71c0261770d/FYP_Logo.png', alt: 'University Final Year Project', name: 'University Final Year Project'},
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/3uqgEDpiPCCkgiaCYEc2AQ/808d4fe13f39001a05dc3872042016bc/ccdesign.png', alt: 'CCDesign Website Logo', name: 'CCDesign Website Logo'} 
+  ]
   ngOnInit() {
   }
 
