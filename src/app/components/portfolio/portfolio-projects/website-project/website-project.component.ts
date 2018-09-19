86e833bb25e22cde26accfaa6f20b6f81823626e
Michael Beaseley <m.t.beaseley@gmail.com>
2018-09-19 21:16:51 +0100

    dev: fix some styling bugs plus meta data

--- src/app/components/portfolio/portfolio-projects/website-project/website-project.component.ts
+++ src/app/components/portfolio/portfolio-projects/website-project/website-project.component.ts
@@ -8,9 +8,9 @@ import { AppGlobals } from '../../../../app.global';
 })
 export class WebsiteProjectComponent implements OnInit {
   imageUrls = [
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/2olnrwDLIgUEggOMEKOOCo/3fe87a89706a21865de5e16e3ce8b697/fyp_development_process.png'},
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/19NvMzaVtOS8IsKkYeAwGi/1b266aca6a171f9f99a7708aa722c2c9/fyp_software_development.png'},
-    {url: 'https://images.ctfassets.net/43i5e5k8e66i/6OIgcSCzzGe0ck6EeSQCww/19d9b65f92eeffcc7179cdd7293a42ee/fyp_cpu_testing.png'}
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/3FmMgMOcOccuoM0M6ey2su/54aa1a454cb83a86bd5a86e1e0b86998/website_digital_plan.png'},
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/3FmMgMOcOccuoM0M6ey2su/54aa1a454cb83a86bd5a86e1e0b86998/website_digital_plan.png'},
+    {url: 'https://images.ctfassets.net/43i5e5k8e66i/3FmMgMOcOccuoM0M6ey2su/54aa1a454cb83a86bd5a86e1e0b86998/website_digital_plan.png'}
   ];
 
   constructor(public global: AppGlobals) {
