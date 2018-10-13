6bcdddc5dcbf0ed03f4738fe9845ea1ed7b2d571
Michael Beaseley <m.t.beaseley@gmail.com>
2018-10-13 15:33:04 +0100

    dev: update description in meta data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
 
   constructor(private meta: Meta, public global: AppGlobals) {
     this.meta.addTags([
-      {name: 'description', content: 'Personal CV based Website'},
+      {name: 'description', content: 'Personal CV based Website. See any previous projects. Get to know who is Michael Beaseley and what he has got to offer.'},
       {name: 'author', content: 'Michael Beaseley'},
       {name: 'image', content: 'https://images.ctfassets.net/43i5e5k8e66i/4O1Qth30iI6uOgQy88eoEi/9e82a8072453dadd927df09a146be1c2/website_pic.jpg'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'}
