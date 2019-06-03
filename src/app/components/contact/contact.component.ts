5b31df486ab539bcd06f8b3b12013b9939b8a5c2
Michael Beaseley <michael.beaseley@talktalkplc.com>
2019-06-03 17:00:09 +0100

    dev: change images to be more compressed

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,13 +7,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ContactComponent implements OnInit {
-  contentfulUrl = 'https://images.ctfassets.net/43i5e5k8e66i/';
-
-  imageUrls = [
-    {url: this.contentfulUrl +  '2tbgM7o5Wk6208QeuQM6o0/e40591acca2af7f3eb835a1d3bc57fc2/profile_pic.jpg'},
-    {url: this.contentfulUrl +  'pj0mHN9DtQUg4gOuqm8KE/c4b222cd1703b4f608d68fabc4ccef0a/profile_station.jpg'},
-  ];
-
   constructor() {}
 
   ngOnInit() {}
